e62d3b61f8169bb45365476119af0392970767b1
jangmyungdeung <jangmyungdeung@gmail.com>
2023-04-12 14:30:01 +0700

    TEST

--- src/FileJava/BorrowerController.java
+++ src/FileJava/BorrowerController.java
@@ -32,6 +32,75 @@ public class BorrowerController implements Initializable{
     @FXML private Scene scene;
 
     @FXML
+    private TextField cardIdField;
+
+    @FXML
+    private TextField borrowerIdField;
+
+    @FXML
+    private DatePicker borrowDateField;
+
+    @FXML
+    private DatePicker returnDueDateField;
+    
+    @FXML
+    public void processReturn(ActionEvent event) throws IOException {
+        String cardId = cardIdField.getText();
+        String borrowerId = borrowerIdField.getText();
+        LocalDate borrowDate = borrowDateField.getValue();
+        LocalDate returnDueDate = returnDueDateField.getValue();
+
+        if (validateInput(cardId, borrowerId, borrowDate, returnDueDate)) {
+            // Xử lý việc trả sách ở đây
+
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Thông báo");
+            alert.setHeaderText(null);
+            alert.setContentText("Trả sách thành công!");
+            alert.showAndWait();
+
+            // Chuyển về trang chủ sau khi trả sách thành công
+            switchToHome(event);
+        }
+    }
+
+    // kiểm tra và thông báo nếu nhập sai hoặc thiếu thông tin
+    private boolean validateInput(String cardId, String borrowerId, LocalDate borrowDate, LocalDate returnDueDate) {
+        String errorMessage = "";
+
+        if (cardId == null || cardId.trim().isEmpty()) {
+            errorMessage += "Vui lòng nhập ID thẻ mượn sách\n";
+        }
+
+        if (borrowerId == null || borrowerId.trim().isEmpty()) {
+            errorMessage += "Vui lòng nhập ID người mượn sách\n";
+        }
+
+        if (borrowDate == null) {
+            errorMessage += "Vui lòng chọn ngày mượn\n";
+        }
+
+        if (returnDueDate == null) {
+            errorMessage += "Vui lòng chọn ngày hẹn trả\n";
+        }
+
+        if (returnDueDate != null && returnDueDate.isBefore(borrowDate)) {
+            errorMessage += "Ngày hẹn trả không hợp lệ\n";
+        }
+
+        if (errorMessage.isEmpty()) {
+            return true;
+        } else {
+            Alert alert = new Alert(Alert.AlertType.ERROR);
+            alert.setTitle("Lỗi");
+            alert.setHeaderText(null);
+            alert.setContentText(errorMessage);
+            alert.showAndWait();
+            return false;
+        }
+    }
+    
+    @FXML
     public void switchToHome(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/FileFXML/HomeScene.fxml"));
         stage = (Stage)((Node)event.getSource()).getScene().getWindow();
@@ -256,4 +325,6 @@ public class BorrowerController implements Initializable{
             successAlert.showAndWait();
         }
     }
+
+
 }
