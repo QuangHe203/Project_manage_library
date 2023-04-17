e57d01f72933c345df16eb2e273e20a26929e77d
Phu-Akira <21010654@st.phenikaa-uni.edu.vn>
2023-04-17 15:57:49 +0700

    addbd

---
+++ database/MySQLConnection.java
@@ -0,0 +1,24 @@
+package database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class MySQLConnection {
+    
+    // Thông tin kết nối database MySQL
+    private static final String URL = "jdbc:mysql://localhost:3306/library";
+    private static final String USER = "root";
+    private static final String PASSWORD = "Akira123456.";
+
+    // Phương thức thiết lập kết nối với database MySQL
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+        } catch (ClassNotFoundException ex) {
+            throw new SQLException("MySQL JDBC driver not found.", ex);
+        }
+    }
+}
+
